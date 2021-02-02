66d8ba1446af80c7ee57f8196eb4d1737e82b649
Xavier Johanis Elnas <xavierjohaniselnas@Xaviers-Mac.local>
2021-02-02 18:47:02 +0800

    initialize

---
+++ iTunesSearchAPI/iTunesSearchAPI/mainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  mainViewController.swift
+//  iTunesSearchAPI
+//
+//  Created by Xavier Johanis Elnas on 02/02/2021.
+//  Copyright © 2021 Xavier Johanis Elnas. All rights reserved.
+//
+
+import UIKit
+
+class mainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
